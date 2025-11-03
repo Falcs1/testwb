9ac60b6abf6989ed2e993e3a04fbb0424897cb20
falcs1 <sqlrproject@gmail.com>
2025-11-03 20:15:06 +0100

    Remove team section completely

--- zirox/src/components/home1/Team.tsx
+++ zirox/src/components/home1/Team.tsx
@@ -1,21 +1,5 @@
-import Image from "next/image";
-
 const TeamMembers = () => {
-  return (
-    <section className="zirox-team zirox-team--home-2 zirox-section">
-      <div className="container">
-        <h6 data-aos="fade-up" className="text-center">
-          Our Team
-        </h6>
-        <h2 data-aos="fade-up" data-aos-delay="300" className="text-center ">
-          Meet Our Professional Consultants
-        </h2>
-        <div className="row">
-          {/* Team members removed */}
-        </div>
-      </div>
-    </section>
-  );
+  return null; // Team section completely removed
 };
 
 export default TeamMembers;
