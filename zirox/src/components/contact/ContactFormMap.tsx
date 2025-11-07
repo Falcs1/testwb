ae0a41b5143f9e088e226b50e2894ccbe9a7a55f
falcs1 <sqlrproject@gmail.com>
2025-11-07 18:04:52 +0100

    Update contact map to correct coordinates (42.657, 21.152)

--- zirox/src/components/contact/ContactFormMap.tsx
+++ zirox/src/components/contact/ContactFormMap.tsx
@@ -3,7 +3,8 @@ const ContactFormMap = () => {
     <section className="zirox-contact zirox-contact--single">
       <div className="zirox-contact__map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.1573856871835!2d21.165306315674!3d42.66346147916724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee605110927%3A0x9365bfdf385eb95a!2sMarigona%20Tower!5e0!3m2!1sen!2s!4v1634567890123!5m2!1sen!2s"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.2!2d21.151949210714154!3d42.65698338360255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDM5JzI1LjEiTiAyMcKwMDknMDcuMCJF!5e0!3m2!1sen!2s!4v1634567890123!5m2!1sen!2s"
+          width="100%"
           height={450}
           style={{ border: 0 }}
           loading="lazy"
